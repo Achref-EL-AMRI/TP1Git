cc36a2913caaf5d81a1a17b8bd2c0918ab097932
Achref-EL-AMRI <achraf.elamri@gmail.com>
2024-02-14 16:50:27 +0100

    first commit system_hospitalier.java

--- System_hospitalier.java
+++ System_hospitalier.java
@@ -34,12 +34,12 @@ public class System_hospitalier {
     }
 
     private String analyserRequete(Requête requête) {
-        
+        //analyser
         return "requête analysée";
     }
 
     private String comparerDonnes() {
-        
+        //comparer
         return "Cluster trouvé";
     }
 
